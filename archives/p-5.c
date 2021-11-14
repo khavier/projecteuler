6aa673a1d4c30326dcc274e6f6a1d95fd0e8f2b7
neviser <gorgdis@gmail.com>
2021-11-14 19:42:04 +0330

    update archives

---
+++ archives/p-5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int limit  = 20;
+    int output = 1;
+    int status = 1;
+    int i;
+
+    while (1) {
+
+        for (i = 1; i <= limit; i++) {
+
+            if (output % i != 0)
+                status = 0;
+
+        }
+
+        if (status == 1)
+            break;
+
+        output += 1;
+        status  = 1;
+    }
+
+    printf("Output: %d\n", output);
+
+    return 0;
+}
